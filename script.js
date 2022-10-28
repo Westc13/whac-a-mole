5a345b4c84ab339d14b4242c6f268c9df6fa5580
ChristineWest <christinewwwest@gmail.com>
2022-10-27 20:41:53 -0400

    added timer count down

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 
 let result = 0;
+let timerId = null;
 let hitPosition;
+let currentTime = 60;
 function randomSquare() {
   squares.forEach((square) => {
     square.classList.remove("mole");
@@ -25,7 +27,18 @@ squares.forEach((square) => {
   });
 });
 function moveMole() {
-  let timerId = null;
   timerId = setInterval(randomSquare, 500);
 }
 moveMole();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+  if (currentTime === 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert(`GAME OVER! Your final score is ${result}`);
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
